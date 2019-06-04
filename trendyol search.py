ece58c7f5aabb7844d5be2bdfe555deec0c8dc7f
emreozincegedik <emreozincegedik@gmail.com>
2019-06-04 21:42:11 +0300

    testt

--- trendyol search.py
+++ trendyol search.py
@@ -7,7 +7,7 @@ html_kodu =site.content #içeriğini alıyoruz
 islenmis_html=BeautifulSoup(html_kodu,features="html.parser") 
 
 urun_isimleri=islenmis_html.find_all('span', attrs={'class':'prdct-desc-cntnr-name'}) 
-urun_fiyatlari=islenmis_html.find_all('div', attrs={'class':'prc-box-sllng'}) #ürün fiyatlarını alıyoruz class 'prc-box...'
+urun_fiyatlari=islenmis_html.find_all('div', attrs={'class':'prc-box-sllng'}) 
 
 urun_linkleri=islenmis_html.find_all('a',attrs={'class':'p-card-chldrn-cntnr'}) #ürün linklerini alıyoruz
 i=1
