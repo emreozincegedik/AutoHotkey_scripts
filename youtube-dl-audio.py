3417e58d7a90a677ce9a0048ebaafee0972c78b7
emreozincegedik <emreozincegedik@gmail.com>
2019-09-22 22:38:32 +0300

    $

---
+++ youtube-dl-audio.py
@@ -0,0 +1,8 @@
+import subprocess
+import tkinter
+r=tkinter.Tk()
+r.withdraw()
+print(r.clipboard_get())
+subprocess.call('youtube-dl -o "D:\\Torrent\\%(title)s.%(ext)s" -x --audio-format "mp3"  "'+r.clipboard_get()+'"')
+
+#youtube-dl -o "D:\Torrent\%(title)s.%(ext)s" -x --audio-format "mp3"  "$(Get-Clipboard)" #for powershell only
